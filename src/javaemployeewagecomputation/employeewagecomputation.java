4721268f599ded9d96975c7eab5cbfcfb6cd3440
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-23 02:50:03 +0530

    employee wage

--- src/javaemployeewagecomputation/employeewagecomputation.java
+++ src/javaemployeewagecomputation/employeewagecomputation.java
@@ -3,8 +3,14 @@ package javaemployeewagecomputation;
 public class employeewagecomputation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int empCheck = (int)Math.floor(Math.random() * 10) % 2;
+		if (empCheck == 1) {
+			System.out.println("Employee is Present");
+		}else {
+		System.out.println("Employee is Absent");
+		}
+	}
+
 
 	}
 
-}
